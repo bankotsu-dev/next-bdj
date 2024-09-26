3623ec959032a333e3cc69b62b3fa9453455ad76
bankotsu-dev <bankotsugamer@gmail.com>
2024-09-25 22:37:15 -0700

    Se agrego fondo a la page de dd/regiones

--- app/darkest-dungeon/clases/page.tsx
+++ app/darkest-dungeon/clases/page.tsx
@@ -7,9 +7,9 @@ export default function Page() {
     <>
       <h1>Clases Page</h1>
       <CldImage
-        src="https://res.cloudinary.com/bdj/image/upload/v1727233187/NAMI_WANTED_mb2tji.jpg"
-        width={80}
-        height={80}
+        src="NAMI_WANTED_mb2tji"
+        width={403}
+        height={581}
         alt="ant"
         priority
       />
--- app/darkest-dungeon/regiones/page.tsx
+++ app/darkest-dungeon/regiones/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { CldImage } from 'next-cloudinary';
 
 export default function Page() {
     return (
       <>
+      <CldImage
+        src="dd/regiones/pmosjvxtbkawoqppbzks"
+        width={1920}
+        height={520}
+        alt="ant"
+        priority
+      />
         <h1 className="mb-4 text-xl md:text-2xl">Darkest Dungeon Regiones Page</h1>
       </>
     );
